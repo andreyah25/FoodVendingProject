beb298345ef982bc24b8f6901f41f38e0c628e74
andreyah25 <concepciondeng25@gmail.com>
2025-03-25 20:07:27 +0800

    Method

--- FoodVending.cs
+++ FoodVending.cs
@@ -2,63 +2,90 @@
 
 namespace FoodVendingMachine
 {
-    class FoodVending
+    class Program
     {
+        static double cardBalance = 4.00;
+
         static void Main(string[] args)
         {
-            double cardBalance = 200.00;
-            int pin = 2254;
+
+            Console.WriteLine("WELCOME.");
             Console.WriteLine("Insert your card.");
-            Console.WriteLine("Enter a PIN: ");
-            int userPIN = Convert.ToInt16(Console.ReadLine());
 
-            if (userPIN == pin)
+            while (!ValidatePIN())
             {
-                Console.WriteLine("Snacks or Drinks: ");
-                string[] foods = new string[] { "[1]Piattos- $1.29", "[2]VCut - $2.07", "[3]Cheesy - $1.78", "[4]Pic A - $3.45", "[5]Royal - $2.99", "[6]Mountain Dew -$ 2.56", "[7]CANCEL" };
-                double[] prices = new double[] { 1.29, 2.07, 1.78, 3.45, 2.99, 2.56 };
+                Console.WriteLine("Incorrect PIN. Try Again.");
+            }
+            bool continueTransaction = true;
+            while (continueTransaction)
+            {
+                DisplayMenu();
+                ProceedSelection();
 
-                foreach (string food in foods)
-                {
-                    Console.WriteLine(food);
-                }
-                Console.WriteLine("Pick your choice: ");
-                int userFoodChoice = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Would you like to choose again? (Yes or No): ");
+                string answer = Console.ReadLine();
+                continueTransaction = (answer == "Yes");
+            }
+            Console.WriteLine("Thank you for using the vending machine.");
+        }
+        static bool ValidatePIN()
+        {
+            int pin = 2245;
+            Console.WriteLine("Enter PIN: ");
+            int userPIN = Convert.ToInt16(Console.ReadLine());
+            return userPIN == pin;
+        }
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Snacks and Drinks: ");
+            Console.WriteLine("[1]Piattos- $1.29\n[2]VCut - $2.07\n[3]Cheesy - $1.78\n[4]Pic A - $3.45\n[5]Royal - $2.99\n[6]Mountain Dew - $2.56\n[7]CANCEL\n");
+        }
+        static void ProceedSelection()
+        {
+            Console.WriteLine("Pick your choice: ");
+            int itemName = Convert.ToInt16(Console.ReadLine());
 
-                if (userFoodChoice >= 1 && userFoodChoice <= 6)
-                {
-                    double itemPrice = prices[userFoodChoice - 1];
-                    string itemName = foods[userFoodChoice - 1].Split('-')[0];
+            switch (itemName)
+            {
+                case 1:
+                    Receipt("Piattos", 1.29);
+                    break;
+                case 2:
+                    Receipt("Vcut", 2.07);
+                    break;
+                case 3:
+                    Receipt("Chessy", 1.78);
+                    break;
+                case 4:
+                    Receipt("Pic A", 3.45);
+                    break;
+                case 5:
+                    Receipt("Royal", 2.99);
+                    break;
+                case 6:
+                    Receipt("Mountain Dew", 2.56);
+                    break;
+                case 7:
+                    Console.WriteLine("Cancel");
+                    break;
+                default:
+                    Console.WriteLine("Not Available.");
+                    break;
+            }
+        }
+        static void Receipt(string itemName, double price)
+        {
+            if (cardBalance >= price)
+            {
+                cardBalance -= price;
+                Console.WriteLine($"You selected: {itemName} \nPrice: {price}");
+                Console.WriteLine($"Remaining Balance: ${cardBalance}");
 
-                    if (cardBalance >= itemPrice)
-                    {
-                        cardBalance -= itemPrice;
-                        Console.WriteLine($"You selected {itemName} \n Price: {itemPrice}");
-                        Console.WriteLine($"Current Balance: {cardBalance}");
-                        Console.WriteLine($"Remaining Balance is {cardBalance}");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("Insufficient Funds.");
-
-                    }
-                }
-                else if (userFoodChoice == 7)
-                {
-                    Console.WriteLine("Cancelled.");
-
-                }
-                else
-                {
-                    Console.WriteLine("Not Available, Try Again.");
-                }
             }
             else
             {
-                Console.WriteLine("Access Denied. Incorrect PIN");
+                Console.WriteLine("Insufficient Funds");
             }
-
         }
     }
 }
